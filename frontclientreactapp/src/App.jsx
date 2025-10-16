cf08c48073a964bdc581fffe5a160a2a2510466e
EsnemyC <120020955+Enysmen@users.noreply.github.com>
2025-10-16 19:17:32 +0300

    add form pas and login

--- frontclientreactapp/src/App.jsx
+++ frontclientreactapp/src/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
+import { Tooltip } from 'bootstrap'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,9 +9,21 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      
-        
+      <>
+          <form className="FormPassLogin row g-3" action="" method="POST">
+              <div className="col-sm-3">
+                  <label htmlFor="InputEmail" className="form-label">Email</label>
+                  <input type="email" className="form-control" id="InputEmail" aria-describedby="emailText" />
+              </div>
+              <div className="col-sm-3">
+                  <label htmlFor="InputPassword" className="form-label">Password</label>
+                  <input type="password" className="form-control" id="InputPassword" />
+              </div>
+
+              <button type="submit" className="btn btn-primary col-sm-3">Send</button>
+          </form>
+
+
     </>
   )
 }
